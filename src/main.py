c48ba6b589e78f1126a77a913b16317bcdf0de82
Yusuf Metin ÖZER <yusufmetinozer@gmail.com>
2023-12-10 18:37:06 +0300

    delete model files, update gitignore

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import io
 import os
 import sys
+from tabnanny import check
 sys.path.append(os.getcwd())
 sys.path.append(os.path.abspath(".."))
 
@@ -12,6 +13,7 @@ import torch
 import datasets.BDMediLeaves.BDML_dataset as BDML
 import datasets.kvasir.kvasir as Kvasir
 from torchvision import transforms
+from torchvision.models import resnet50, densenet201, efficientnet_v2_m, EfficientNet_V2_M_Weights
 from torch.utils.data import DataLoader, random_split
 import torch.optim as optim
 import torch.nn as nn
@@ -60,13 +62,109 @@ class SimpleCNN(nn.Module):
         x = nn.functional.softmax(x, dim=1)
         return x
     
+class CNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CNN, self).__init__()
+        self.num_classes = num_classes
+        # Define the feature extraction part
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        
+        # The classifier will be defined later in the forward method
+        self.classifier = None
+
+    def forward(self, x):
+        # Forward pass through the feature extraction part
+        x = self.features(x)
+        
+        # Dynamically obtain the input features for the classifier
+        x_flatten_size = x.view(x.size(0), -1).size(1)
+        
+        # Define the classifier if it hasn't been defined yet
+        if self.classifier is None:
+            self.classifier = nn.Sequential(
+                nn.Linear(x_flatten_size, 1024),
+                nn.ReLU(inplace=True),
+                nn.Dropout(0.5),
+                nn.Linear(1024, self.num_classes),
+            )
+        
+        # Flatten the output for the classification part
+        x = x.view(x.size(0), -1)
+        
+        # Forward pass through the classification part
+        x = self.classifier(x)
+        return x
+
+def train_loop():
+    epoch = 0
+    loss=None
+    step_count = 0
+    try:
+        for epoch in range(num_epochs):
+            train_loss = 0
+            train_acc = 0
+
+            # Training
+            model.train()
+
+            for i, (images, labels) in enumerate(train_loader):
+                step_count = i + 1
+                images = images.to(device)
+
+                # convert all labels to numbers using labels_dict
+                labels = [labels_dict[label] for label in list(labels)]
+                labels = torch.tensor(labels, dtype=torch.long, device=device)
+
+                height, width = images.shape[-2], images.shape[-1]
+
+                # Forward pass
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                train_loss += loss.item()
+
+                # Backward and optimize
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # Track the accuracy
+                total = labels.size(0)
+                _, predicted = torch.max(outputs.data, 1)
+                correct = (predicted == labels).sum().item()
+                train_acc += correct / total
+
+                time_elapsed = time.time() - since
+                left_text = "Epoch [{}/{}], Step [{}/{}] Loss: {:.4f} Acc: {:.2f} images: {}".format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), (correct / total) * 100, tuple(images.shape[2:4]))
+                right_text = "Time elapsed {:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60)
+                # time elapsed
+                print_left_and_right_aligned(left_text, right_text)
+
+            train_loss_list.append(train_loss / len(train_loader))
+            train_acc_list.append(train_acc / len(train_loader))
+        # save model
+        torch.save(model.state_dict(), 'model.ckpt')
+    except KeyboardInterrupt:
+        click.echo(click.style(f"Training interrupted at {step_count}/{total_step}", fg="red"))
+        click.echo(click.style("Saving model", fg="green"))
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+        }, f"model.ckpt")
+        click.echo(click.style("Model saved", fg="green"))
+        sys.exit(0)
+
 
 if __name__ == "__main__":
-
-    # Data augmentation
     train_transform = transforms.Compose([
         transforms.Resize((229, 299)),
         '''
+        # Augmentation
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         transforms.RandomRotation(20),
@@ -111,9 +209,9 @@ if __name__ == "__main__":
     test_loader = DataLoader(dataset=test, batch_size=batch_size, shuffle=False)
     validation_loader = DataLoader(dataset=validation, batch_size=batch_size, shuffle=False)
 
+    '''
     # show images in trainloader according to batch sizes
     for i, (images, labels) in enumerate(train_loader):
-
         print("images shape:\t",images.shape)
         print("labels:\t\t",labels)
         #plot 
@@ -124,13 +222,14 @@ if __name__ == "__main__":
                 ax[i, j].set_title(labels[i*5+j])
                 ax[i, j].axis('off')
         plt.show()
+    '''
             
-
-
-
-
     # Model
-    model = SimpleCNN(num_classes=10)
+    #model = SimpleCNN(num_classes=10)
+    #model = resnet50(pretrained=True)
+    #model = densenet201(pretrained=True)
+    #model = efficientnet_v2_m(weights=EfficientNet_V2_M_Weights.DEFAULT, progress=True)
+    model = CNN(num_classes=10)
     model = model.to(device)
     
     # Loss and optimizer
@@ -160,58 +259,21 @@ if __name__ == "__main__":
     since = time.time()
 
     if os.path.exists("model.ckpt"):
-        model.load_state_dict(torch.load("model.ckpt"))
+        checkpoint = torch.load("model.ckpt")
+        # Update model and optimizer with the loaded state_dicts
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        epoch = checkpoint['epoch']
+        loss = checkpoint['loss']
         click.echo(click.style("Model loaded", fg="green"))
+        model.train()
+        train_loop()
     else:
         click.echo(click.style("Model not found", fg="red"))
         click.echo(click.style("Training model", fg="green"))
-        for epoch in range(num_epochs):
-            train_loss = 0
-            train_acc = 0
-
-            # Training
-            model.train()
-
-            for i, (images, labels) in enumerate(train_loader):
-                images = images.to(device)
-                #print("image shape:\t",images.shape)
-
-                # convert all labels to numbers using labels_dict
-                labels = [labels_dict[label] for label in list(labels)]
-                labels = torch.tensor(labels, dtype=torch.long, device=device)
-                #print("label shape:\t",labels.shape)
-
-                height, width = images.shape[-2], images.shape[-1]
-
-                # Forward pass
-                outputs = model(images, height, width)
-                #print("output shape:\t",outputs.shape)
-
-
-                loss = criterion(outputs, labels)
-                train_loss += loss.item()
-
-                # Backward and optimize
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-                # Track the accuracy
-                total = labels.size(0)
-                _, predicted = torch.max(outputs.data, 1)
-                correct = (predicted == labels).sum().item()
-                train_acc += correct / total
-
-                time_elapsed = time.time() - since
-                left_text = "Epoch [{}/{}], Step [{}/{}] Loss: {:.4f} Acc: {:.2f} images: {}".format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), (correct / total) * 100, tuple(images.shape[2:4]))
-                right_text = "Time elapsed {:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60)
-                # time elapsed
-                print_left_and_right_aligned(left_text, right_text)
-
-            train_loss_list.append(train_loss / len(train_loader))
-            train_acc_list.append(train_acc / len(train_loader))
-        # save model
-        torch.save(model.state_dict(), 'model.ckpt')
+        train_loop()
+    
+    
 
     test_loss = 0
     test_acc = 0
